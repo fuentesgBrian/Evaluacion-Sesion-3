6316cabaaafcaa9d6401229797ab17c668cc4611
fuentesgBrian <fuentesg.brian@gmail.com>
2023-04-14 17:35:25 -0400

    Realizacion Evaluacion Sesion 3

---
+++ index.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+# Definicion variable y
+y = 81 
+
+# Definicion de resultado de la raiz de y a una variable. 
+resultado = sqrt(y)
+
+# Impresion del resultado.
+print(f"El resultado de la raiz cuadrada de {y} es {resultado}")
